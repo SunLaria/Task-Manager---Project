ee05f82653cb44ad6fc57f6b48c0e2bd67eefcb4
RandomG <sun4151@gmail.com>
2024-01-23 11:48:53 +0200

    added GET Tasks to api with optional path queries

--- Task_Manager_app/models.py
+++ Task_Manager_app/models.py
@@ -28,3 +28,15 @@ class Task(models.Model):
     Created_at = models.DateField(auto_now_add=True)
     Updated_at = models.DateField(auto_now=True)
     User = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def to_dict(self):
+        return {
+                "id":self.id,
+                "Name":self.Name,
+                "Date":self.Date,
+                "Category":self.Category.Name,
+                "Tag":self.Tag,
+                "Description":self.Description,
+                "Created_at":self.Created_at,
+                "Updated_at":self.Updated_at,
+                "User":self.User.id}
--- fastapi_api.py
+++ fastapi_api.py
@@ -4,7 +4,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'Taskmanager.settings')
 django.setup()
 
 
-from fastapi import FastAPI,HTTPException, Request,Body
+from fastapi import FastAPI,HTTPException, Request,Body, Query
 from starlette import status
 from pydantic import BaseModel, Field
 from typing import Annotated
@@ -13,12 +13,23 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse 
 from django import db
+from django.http import JsonResponse as DjangoJsonResponse
 from Task_Manager_app.models import Task, Category
 from django.contrib.auth.models import User
 
 
 
-
+#   "id": 17,
+#       "Name": "Clean bad",
+#       "Date": "2024-01-28",
+#       "Category": "home",
+#       "Tag": "high-priority",
+#       "Description": "Asap",
+#       "Created_at": "2024-01-22",
+#       "Updated_at": "2024-01-22",
+#       "User": 1
+#     }
+#   ]
 
 
 class Task_Schema(BaseModel):
@@ -28,6 +39,9 @@ class Task_Schema(BaseModel):
     Category : str |None = Field(default=None,max_length=100)
     Tag: str |None = Field(default=None,max_length=16, examples=["high-priority","medium-priority","low-priority","no-priority"])
     Description : str | None = Field(default=None)
+    Created_at : date | None = Field(default=None)
+    Updated_at : date | None = Field(default=None)
+    User : int | None = Field(default=None)
     model_config = {
         "json_schema_extra": {
             "examples": [
@@ -53,7 +67,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 
 @app.post("/create-task", status_code = status.HTTP_201_CREATED, tags=["Task"])
-def create_task(user_id:Annotated[int,Body(ge=1,embed=True,example=1)] ,task:Task_Schema):
+def create_task(user_id:Annotated[int,Body(ge=1,example=1)] ,task:Task_Schema):
     try:
         data=jsonable_encoder(task.model_dump(exclude_unset=True))     
         new_task = Task(Name=data["Name"],Date=data["Date"],Category=User.objects.get(id=user_id).category_set.get_or_create(Name=data["Category"])[0],Tag=data["Tag"],Description=data["Description"],User=User.objects.get(id=user_id))
@@ -64,7 +78,7 @@ def create_task(user_id:Annotated[int,Body(ge=1,embed=True,example=1)] ,task:Tas
         raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail="Task Creation Failed")
     
 @app.patch("/update-task", status_code = status.HTTP_202_ACCEPTED, tags=["Task"])
-def update_task(user_id:Annotated[int,Body(ge=1,embed=True,example=1)], task_id:Annotated[int,Body(ge=1,embed=True,example=1)] ,task:Task_Schema):
+def update_task(user_id:Annotated[int,Body(ge=1,example=1)], task_id:Annotated[int,Body(ge=1,example=1)] ,task:Task_Schema):
     try:
         update_data=jsonable_encoder(task.model_dump(exclude_unset=True)) 
         task_db=Task.objects.get(id=task_id)
@@ -81,4 +95,36 @@ def delete_task(tasks_id:Annotated[list[int],Body()]):
         return JSONResponse(content={"detail":"Task Delete Succesfull"},status_code=status.HTTP_200_OK)
     except:
         raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail="Task Delete Failed")
-        
+
+@app.get("/read-task",status_code=status.HTTP_202_ACCEPTED,tags=["Task"])
+def get_tasks(user_id:Annotated[int | None, Query(ge=1,example=1)] = None, task_id:Annotated[int | None,Query(ge=1,example=1)] = None, tag: Annotated[str | None,Query(pattern='(high-priority|medium-priority|low-priority|no-priority)')] = None):
+    if task_id:
+        try:
+            result = {"result":[i.to_dict() for i in Task.objects.filter(id=task_id)]}
+            if len(result["result"]) > 0:
+                return result
+            else:
+                return {"result":"No Data Found"}
+        except:
+            raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail="Failed To Retrive Data From DB")
+    elif user_id and tag:
+        try:
+            result = {"result":[i.to_dict() for i in Task.objects.filter(User=user_id,Tag=tag)]}
+            if len(result["result"]) > 0:
+                return result
+            else:
+                return {"result":"No Data Found"}
+        except:
+            raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail="Failed To Retrive Data From DB")
+   
+    elif user_id:
+        try:
+            result = {"result":[i.to_dict() for i in Task.objects.filter(User=user_id)]}
+            if len(result["result"]) > 0:
+                return result
+            else:
+                return {"result":"No Data Found"}
+        except:
+            raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail="Failed To Retrive Data From DB")
+    else:
+        raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail="Failed To Retrive Data From DB")
