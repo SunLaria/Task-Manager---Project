d08bcad420519188b76fcee98a9c72c81cbd0c6e
RandomG <sun4151@gmail.com>
2024-01-30 20:11:56 +0200

    preconfigured db with admin user, fixed test_api to work

--- README.md
+++ README.md
@@ -10,12 +10,24 @@ SPA Site written mainly in JS.
 
 How to build:
 - pip install -r requirements.txt
-- python ./manage.py makemigrations
-- python ./manage.py migrate
-- Run BOTH FastAPI and Django debug server 
+- Run BOTH FastAPI and Django debug server, with the configurations provided.
+
+How to test API:
+- Djnago db task_id is ++1 after each task creation.
+- Admin User = admin:admin
+- You can test Create, Read, Update, Delete functions
+- Test Creates Task, Update the Task, Read The Task, Then delete the task.
+- You can run test_api using pytest once, beacuse the next task will be task_id=2 (it can be configured in test_api.py file). 
+
 
 How to Use:
-- Create Task using Create Button.
-- Double click on task to see more details and options.
+- Admin User = admin:admin
+- Complex Validators in Register, login Routes And in Create, Update Task Tab.
+- Create Task using Create Button
+- Double click on task to see more details and options such ass: Delete, Update
+- Task Table can be sorted while clicking specific headers.
+- You can select multiple tasks to delete from the Tasks Table.
+- User Past Categories are provided in category input you can choose or else you can create a new one.
+
 
 WIP - Task Share option.
--- test_api.py
+++ test_api.py
@@ -2,6 +2,8 @@ import requests
 import pytest
 url = "http://127.0.0.1:7000"
 
+task_id = 1
+
 def test_create():
     data = {
   "user_id": 1,
@@ -10,7 +12,7 @@ def test_create():
     "Date": "2024-01-28",
     "Description": "Asap",
     "Name": "Clean bad",
-    "Tag": "high-priority"
+    "Tag": "High Priority"
   }}
     data_error={
   "user_id": 1,
@@ -21,7 +23,7 @@ def test_create():
     "Name": "Clean bad",
     "Tag": "high"
   }}
-    assert requests.post(url=url+"/create-task",json=data).json()["detail"]=="Task Creation Succesfull"
+    assert requests.post(url=url+"/create-task",json=data).json()["detail"]=="Task Creation Successful"
 
     false_request = requests.post(url=url+"/create-task",json=data_error)
     assert false_request.status_code == 422
@@ -30,18 +32,19 @@ def test_create():
 def test_update():
     data = {
   "user_id": 1,
-  "task_id": 17,
+  "task_id": task_id,
   "task": {
-    "Category": "home",
+    "Category": "Room",
     "Date": "2024-01-28",
     "Description": "Asap",
     "Name": "Clean bad",
-    "Tag": "low-priority"
+    "Tag": "Low Priority"
   }
 }
+
     data_eror ={
   "user_id": 1,
-  "task_id": 17,
+  "task_id": task_id,
   "task": {
     "Category": "home",
     "Date": "dasda",
@@ -50,31 +53,16 @@ def test_update():
     "Tag": "priority"
   }
 }
-    assert requests.patch(url=url+"/update-task",json=data).json()["detail"]=="Task Update Succesfull"
+    
+    positive_request =requests.patch(url=url+"/update-task",json=data)
+    assert positive_request.status_code== 202
 
     false_request = requests.patch(url=url+"/update-task",json=data_eror)
     assert false_request.status_code == 422
         
 
-# trouble testing multiple times as it deletes the task. the test is working.
-def test_delete():
-    data = [28]
-    data_not_exist=[1000000]
-    data_error=["ds"]
-    # delete
-    assert requests.delete(url=url+"/delete-task",json=data).json()["detail"]=="Task Delete Succesfull"
-    # not exist
-    false_request = requests.delete(url=url+"/delete-task",json=data_not_exist)
-    assert false_request.json()["detail"] == "Task Delete Failed"   
-    # validation error
-    error_request = requests.patch(url=url+"/update-task",json=data_error)
-    assert error_request.status_code == 422
-
-
 def test_read():
-    task_id=24
     user_id=1
-    tag="high-priority"
 
     assert type(requests.get(url=url+f"/read-task?user_id={user_id}").json()["result"])==list
     assert type(requests.get(url=url+f"/read-task?task_id={task_id}").json()["result"])==list
@@ -84,4 +72,16 @@ def test_read():
     # no data according to input
     assert requests.get(url=url+f"/read-task?task_id=10000000").json()["result"]=="No Data Found"
 
-
+# one time test
+def test_delete():
+    data = [task_id]
+    data_not_exist=[1000000]
+    data_error=["ds"]
+    # delete
+    assert requests.delete(url=url+"/delete-task",json=data).json()["detail"]=="Task Delete Successful"
+    # not exist
+    false_request = requests.delete(url=url+"/delete-task",json=data_not_exist)
+    assert false_request.json()["detail"] == "Task Delete Failed"   
+    # validation error
+    error_request = requests.patch(url=url+"/update-task",json=data_error)
+    assert error_request.status_code == 422
